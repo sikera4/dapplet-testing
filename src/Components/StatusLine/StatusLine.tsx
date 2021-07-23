be82bc3ecf553284cb594e21f9f9cef05b124528
Andrew <sikera0812@mail.ru>
2021-07-23 17:30:25 +0200

    - finish status line

--- src/Components/StatusLine/StatusLine.tsx
+++ src/Components/StatusLine/StatusLine.tsx
@@ -1,5 +1,6 @@
 import './StatusLine.scss';
 import cloud from '../../assets/clarity_cloud-network-line.png';
+import settings from '../../assets/carbon_settings-adjust.png';
 
 const StatusLine = () => {
   return (
@@ -8,6 +9,10 @@ const StatusLine = () => {
         <img src={cloud} alt="" className="status-line__cloud" height="16" width="18"/>
         <span className="status-line__state">Extension state: </span>
         <span className="status-line__status"> Active</span>
+      </div>
+      <div className="status-line__settings">
+        <img src={settings} alt="" className="status-line__settings-icon" width="20" height="18"/>
+        <span className="status-line__settings-caption">Settings</span>
       </div>
     </div>
   )
